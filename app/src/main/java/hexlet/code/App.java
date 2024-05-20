1603a2744300138c17ba05d5d893dbb7b57e95d6
Valentin Shabarchin <dronanval@gmail.com>
2024-05-20 12:41:06 +0400

    ADD codebase for database support

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,23 +1,63 @@
 package hexlet.code;
 
-import io.javalin.Javalin;
+// Java built-in libs
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
 
+// App classes imports
+import hexlet.code.repository.BaseRepository;
+
+// Hikari config
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+// Javalin and Slf4j
+import io.javalin.Javalin;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
-
-    public static void main(String[] args) {
-        var app = getApp();
-        app.start(getPort());
-    }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
     }
 
-    public static Javalin getApp() {
+    private static String getDbUrl() {
+        return System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDbUrl());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.setDataSource(dataSource);
+
         var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
 
         app.get("/", ctx -> ctx.result("Hello World"));
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,20 @@
+package hexlet.code.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public final class Url {
+    private Long id;
+
+    private String name;
+    private Timestamp createdAt;
+
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
