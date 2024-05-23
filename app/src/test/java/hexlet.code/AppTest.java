a36d2980289e6a3e80c4e34c7cbb8837bde52f62
Valentin Shabarchin <dronanval@gmail.com>
2024-05-23 12:37:21 +0400

    FIX AppTest.java

--- app/src/test/java/hexlet.code/AppTest.java
+++ app/src/test/java/hexlet.code/AppTest.java
@@ -2,8 +2,14 @@ package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
+
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +30,7 @@ public class AppTest {
     @Test
     public void testMainPage() {
         JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/");
+            var response = client.get(NamedRoutes.rootPath());
             assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
 
             String responseBody = response.body() != null ? response.body().string() : "";
@@ -32,30 +38,36 @@ public class AppTest {
         });
     }
 
-//    @Test
-//    public void testBuildSessionPage() {
-//        JavalinTest.test(app, (server, client) -> {
-//            var response = client.get("/sessions/build");
-//            assertThat(response.code()).isEqualTo(200);
-//        });
-//    }
-//
-//    @Test
-//    public void testUsersPage() {
-//        JavalinTest.test(app, (server, client) -> {
-//            var response = client.get("/users");
-//            assertThat(response.code()).isEqualTo(200);
-//        });
-//    }
-//
-//    @Test
-//    public void testCoursesPage() {
-//        JavalinTest.test(app, (server, client) -> {
-//            var response = client.get("/courses");
-//            assertThat(response.code()).isEqualTo(200);
-//        });
-//    }
-//
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath());
+            assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
+        });
+    }
+
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=https://example.com";
+            var response = client.post(NamedRoutes.urlsPath(), requestBody);
+            assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
+
+            String responseBody = response.body() != null ? response.body().string() : "";
+            assertThat(responseBody).contains("https://example.com");
+        });
+    }
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        Url url = new Url("https://example.com", Timestamp.valueOf(LocalDateTime.now()));
+        UrlsRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlPath(url.getId()));
+            assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
+        });
+    }
+
 //    @Test
 //    public void testBuildCourse() {
 //        JavalinTest.test(app, (server, client) -> {
